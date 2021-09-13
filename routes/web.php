b611c6af471f3c81570e71c9cf7b901cc02a8ec6
SebasR1224 <jsrodriguez6536@misena.edu.co>
2021-09-13 17:31:59 -0500

    cambios en las rutas

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,15 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\BrandController;
 
-Route::get('/', function () {
-    return view('welcome');
-})->middleware('auth');
+
 
 Route::get('/products', [ProductController::class , "show"]);
 Route::get('/product/delete/{id}', [ProductController::class, 'delete'])->name('product.delete');
 Route::get('/product/form/{id?}',[ProductController::class, 'form'])->name('product.form');
 Route::post('/product/save', [ProductController::class, 'save'])->name('product.save');
 
-Auth::routes();
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Auth::routes();
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/brands', [BrandController::class , "show"]);
 Route::get('/brand/delete/{id}', [BrandController::class, 'delete'])->name('brand.delete');
