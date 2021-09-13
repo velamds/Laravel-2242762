7ddb0be0b5c64e38af0c23fb09b708ef745ae1bb
SebasR1224 <jsrodriguez6536@misena.edu.co>
2021-09-13 17:21:52 -0500

    delete routes

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::get('/', function () {
     return view('welcome');
 })->middleware('auth');
 
-Route::get('/products', [ProductController::class , "show"]);
+
 Route::get('/product/delete/{id}', [ProductController::class, 'delete'])->name('product.delete');
 Route::get('/product/form/{id?}',[ProductController::class, 'form'])->name('product.form');
 Route::post('/product/save', [ProductController::class, 'save'])->name('product.save');
@@ -23,11 +23,4 @@ Route::post('/brand/save', [BrandController::class, 'save'])->name('brand.save')
 Route::get('/invoice/{id}', function($id){
     $invoice = App\Models\Invoice::findOrFail($id);
     return dd($invoice->products);
-});
-
-Route::get('/invoice_detail/{inv}/{prod}', function($inv,$prod){
-    $detail = App\Models\InvoiceDetail::where('invoice_id','=',$inv)
-                                        ->where('product_id','=',$prod)
-                                        ->first();
-    return dd($detail);
 });
